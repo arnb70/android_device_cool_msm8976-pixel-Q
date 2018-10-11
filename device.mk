545db8123e828c672c50d2ac391ead4628ae650e
shivatejapeddi <shivatejapeddi@gmail.com>
2018-10-11 04:34:44 +0000

    s2: Use stock EUI offline charging animation
    * Thanks to @CrisBalGreece for extracting the required files from EUI ROM
    
    Change-Id: Ib1d86d76ecc161366bf7c7b7909260732c1d02b4

--- device.mk
+++ device.mk
@@ -175,6 +175,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml
 
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images
+
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
     libcnefeatureconfig
